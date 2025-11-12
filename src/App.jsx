407b7224903ff8f36c26857c95e4bb32e0557aae
Carlos Freitas <carlosfreitassdev@gmail.com>
2025-11-11 21:34:40 -0300

    Navbar e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,32 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 // Importa os componentes de layout
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     // Roteamento
     <Router>
-      {/* Faz a ligação com as páginas */}
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </ul>
+      <Navbar />
 
       {/* Cria um container que aplica um layout */}
       <Container customClass="min-height">
         {/* Especifica as rotas de cada página */}
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
           <Route path="/company" element={<Company />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/newproject" element={<NewProject />}></Route>
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
